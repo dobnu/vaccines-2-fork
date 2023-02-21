d64194fe84c884ea3b27d2b5247fed9256f9ddcd
Dan O <obrien.dan@northeastern.edu>
2023-02-21 13:06:07 -0500

    final improvement

--- src/comparison.py
+++ src/comparison.py
@@ -4,90 +4,45 @@ import numpy as np
 from collections import defaultdict
 import sys
 import seaborn as sns
+import calendar as cal
 
-def fips_to_name(FIPS):
-    map = defaultdict(lambda: FIPS)
-    map.update({
-        '44003': 'Kent County, RI',
-        '01125': 'Tuscaloosa County, AL',
-    })
-    return map[FIPS]
 
-def comparison(FIPS_1='44003', FIPS_2='01125'):
+def comparison(FIPS_list):
+    
     months_df = pd.read_csv("months.csv")
-    months_list = months_df.date.to_list()
 
-    file_list = []
-    for month in months_list:
+
+    df_list = []
+    for month in months_df.date:
         df = pd.read_csv('data/Merge/vaccinations-and-deaths-' + month +'.csv', converters={'FIPS' : str})
-        df['Deaths_Per_1e5'] = df['Deaths'] / df['Census2019_18PlusPop'] * 1e5
-        df['Month'] = month
-        file_list.append(df)
+        df['Date'] = month
+        df_list.append(df[df['FIPS'].isin(FIPS_list)])
 
-
-    merged_df = pd.concat(file_list)
     
-    Kent = merged_df[ merged_df['FIPS'] == '44003']
-    Kent_graph = Kent.loc[: , ['FIPS', "Deaths_Per_1e5", "Month"]]
+    final_df = pd.concat(df_list)
+    final_df['Deaths_per_10k'] = final_df['Deaths']/final_df['Census2019_18PlusPop']*10000
 
-    Tuscaloosa = merged_df[ merged_df['FIPS'] == '01125']
-    Tuscaloosa_graph = Tuscaloosa.loc[: , ['FIPS', "Deaths_Per_1e5", "Month"]]
+    fig, ax1 = plt.subplots()
 
-
-    graph_df = pd.concat([Kent_graph, Tuscaloosa_graph], ignore_index = True)
-    plot = sns.catplot(x= "Month", y="Deaths_Per_1e5", hue = 'FIPS', data = graph_df, kind = 'bar', aspect = 2)
-    plot.set_axis_labels("Months (May 2021 - Dec 2022)", "COVID Deaths (per 100k)")
-
-    # new_labels = [(label.get_text()[:3] + label.get_text()[8:]) for label in plot.get_xticklabels()]
-    new_labels = [month[:3] + month[8:] for month in months_list]
-    plot.set_xticklabels(labels = new_labels)
-
-    plot.savefig("img/comp.png")
-
-
-    """
-    cleaned_df = merged_df[merged_df['FIPS'].str.contains(FIPS_1+"|"+FIPS_2)]
-
-    FIPS_1_name = fips_to_name(FIPS_1)
-    FIPS_2_name = fips_to_name(FIPS_2)
-
-    print("Comparing "+FIPS_1_name+" and "+FIPS_2_name)
-    FIPS_1_vax = cleaned_df.loc[cleaned_df['FIPS']==FIPS_1]
-    FIPS_1_vax = FIPS_1_vax["Series_Complete_18PlusPop_Pct"]
-    FIPS_1_death = cleaned_df.loc[cleaned_df['FIPS']==FIPS_1]
-    FIPS_1_death = FIPS_1_death['Deaths_Per_1e5']
-    
-    FIPS_2_vax = cleaned_df.loc[cleaned_df['FIPS']==FIPS_2]
-    FIPS_2_vax = FIPS_2_vax["Series_Complete_18PlusPop_Pct"]
-    FIPS_2_death = cleaned_df.loc[cleaned_df['FIPS']==FIPS_2]
-    FIPS_2_death = FIPS_2_death['Deaths_Per_1e5']
-
-    fig,ax1 = plt.subplots() 
-    x = [0, 1, 2, 3, 4, 5, 6]
-    labels = ['May', 'June', 'July', 'August', 'September', 'October', 'November']
-    x = np.arange(len(labels))
-    width = 0.35 
-    ax1.set_xticks(x, labels)
-    ax1.set_ylim(0, 100)
-    ax1.set_xlim(0,6)
-    ax1.set_title('18+ Vaccination Rates and Deaths in '+FIPS_1_name+" and "+FIPS_2_name, wrap=True)
-    ax1.set_xlabel('Month 2021')
-    ax1.set_ylabel('Percent Vaccinated') 
-    ax1.bar(x - width/2, FIPS_1_vax, width, color='darkcyan', label = '% Vaccinated in '+FIPS_1_name)
-    ax1.bar(x + width/2, FIPS_2_vax, width, color='indianred', label = '% Vaccinated in '+FIPS_2_name)
+    ax1 = sns.barplot(data = final_df, x = 'Date', y = 'Deaths_per_10k', hue = 'Recip_County')
     ax2 = ax1.twinx()
-    ax2.plot(x, FIPS_1_death, color='darkslategray', label = '# of Deaths in '+FIPS_1_name)
-    ax2.plot(x, FIPS_2_death, color='firebrick', label = '# of Deaths in '+FIPS_2_name)
-    ax2.set_ylabel('Deaths per 100k Population.')
-    handles_1, _ = ax1.get_legend_handles_labels()
-    handles_2, _ = ax2.get_legend_handles_labels()
-    handles_2.extend(handles_1) # adds the list items from handles_1 to handles_2
-    ax2.legend(handles = handles_2, loc='upper left')
-    fig.savefig("img/comparison.png")
-    """
+    sns.lineplot(data= final_df, x = 'Date', y = 'Series_Complete_18PlusPop_Pct', hue = 'Recip_County', legend = False)
+    #customize
+    new_labels = [cal.month_abbr[int(label.get_text()[:2])] + " '" + label.get_text()[8:] for label in ax1.get_xticklabels()]
+    ax1.set_xticks(range(len(new_labels)), labels = new_labels)
+    # ax1.set_aspect(1.5)
+    ax1.legend(title = "County")
+    ax1.set_ylabel("Deaths (per 10k)")
+    ax2.set_ylabel("Vaccination Rate (%)")
+
+    
+    fig.set_size_inches(18.5, 10.5)
+    plt.savefig("img/fig2.png", dpi = 100)
+
+    
 
 def main(argv):
-    comparison(argv[0], argv[1])
+    comparison([argv[0], argv[1]])
 
 if __name__ == "__main__":
     argv = sys.argv[1:]
